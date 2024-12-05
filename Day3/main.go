ca8147daae3e25da982fe7a5a3de1368ed286520
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2024-12-05 16:09:25 +0900

    temp

--- Day3/main.go
+++ Day3/main.go
@@ -4,10 +4,41 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan();
-	fmt.Println(scanner.Text())
+	scanner.Scan()
+	input := scanner.Text()
+	count := 0
+
+	for strings.Index(input, "mul(") != -1 {
+		start := strings.Index(input, "mul(")
+		mid := start + strings.Index(input[start:], ",")
+		end := mid + strings.Index(input[mid:], ")")
+
+		firstPart, err := strconv.Atoi(input[start+len("mul(") : mid])
+		if err != nil {
+			input = input[end+1:]
+			println("the former is not a number")
+			continue
+		} else {
+			fmt.Println(firstPart)
+		}
+
+		secondPart, err := strconv.Atoi(input[mid+1 : end])
+
+		if err != nil {
+			input = input[end+1:]
+			println("the latter is not a number")
+			continue
+		} else {
+			fmt.Println(secondPart)
+		}
+		count += firstPart + secondPart
+		input = input[end+1:]
+	}
+	fmt.Printf("the total is %v\n", count)
 }
