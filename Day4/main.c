e8bdbb846815b708e2a85c17cf5a33c77820c8b0
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2024-12-05 13:38:22 +0900

    complete Day4

--- Day4/main.c
+++ Day4/main.c
@@ -2,57 +2,32 @@
 #include <string.h>
 
 int main() {
-    char field[1000][1000];
-    int height = 0;
-    int width = 0;
-    int count = 0;
-    while (fgets(field[height], sizeof(field[height]), stdin) != NULL) {
-        height++;
-    }
-    width = strlen(field[0]);
-
-    //左右
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width - 3; x++) {
-            if (field[y][x] == 'X' && field[y][x + 1] == 'M' && field[y][x + 2] == 'A' && field[y][x + 3] == 'S') {
-                count++;
-            } else if (field[y][x] == 'S' && field[y][x + 1] == 'A' && field[y][x + 2] == 'M' && field[y][x + 3] == 'X') {
-                count++;
-            } 
+  char field[1000][1000];
+  int height = 0;
+  int width = 0;
+  int count = 0;
+  while (fgets(field[height], sizeof(field[height]), stdin) != NULL) {
+    height++;
+  }
+  width = strlen(field[0]);
+  for (int y = 1; y < height - 1; y++) {
+    for (int x = 1; x < width - 1; x++) {
+      if (field[y][x] == 'A') {
+        char ul = field[y - 1][x - 1];
+        char ur = field[y - 1][x + 1];
+        char ll = field[y + 1][x - 1];
+        char lr = field[y + 1][x + 1];
+        if (ul == 'M' && lr == 'S' && ur == 'M' && ll == 'S') {
+          count++;
+        } else if (ul == 'M' && lr == 'S' && ur == 'S' && ll == 'M') {
+          count++;
+        } else if (ul == 'S' && lr == 'M' && ur == 'M' && ll == 'S') {
+          count++;
+        } else if (ul == 'S' && lr == 'M' && ur == 'S' && ll == 'M') {
+          count++;
         }
+      }
     }
-
-    //上下
-    for (int x = 0; x < width; x++) {
-        for (int y = 0; y < height - 3; y++) {
-            if (field[y][x] == 'X' && field[y + 1][x] == 'M' && field[y + 2][x] == 'A' && field[y + 3][x] == 'S') {
-                count++;
-            } else if (field[y][x] == 'S' && field[y + 1][x] == 'A' && field[y + 2][x] == 'M' && field[y + 3][x] == 'X') {
-                count++;
-            }
-        }
-    }
-
-    //左上から右下
-    for (int y = 0; y < height - 3; y++) {
-        for (int x = 0; x < width - 3; x++) {
-            if (field[y][x] == 'X' && field[y + 1][x + 1] == 'M' && field[y + 2][x + 2] == 'A' && field[y + 3][x + 3] == 'S') {
-                count++;
-            } else if (field[y][x] == 'S' && field[y + 1][x + 1] == 'A' && field[y + 2][x + 2] == 'M' && field[y + 3][x + 3] == 'X') {
-                count++;
-            }
-        }
-    }
-
-    //右上から左下
-    for (int y = 3; y < height; y++) {
-        for (int x = 0; x < width - 3; x++) {
-            if (field[y][x] == 'X' && field[y - 1][x + 1] == 'M' && field[y - 2][x + 2] == 'A' && field[y - 3][x + 3] == 'S') {
-                count++;
-            } else if (field[y][x] == 'S' && field[y - 1][x + 1] == 'A' && field[y - 2][x + 2] == 'M' && field[y - 3][x + 3] == 'X') {
-                count++;
-            }
-        }
-    }
-    printf("%d\n", count);
+  }
+  printf("%d\n", count);
 }
